6cd7800c4023d20705adae96a8c0d1791e41241e
tiger <thisisstreamliner@gmail.com>
2022-11-08 23:09:55 +0200

    C++ Files ad Begin Play add more variables and calculate

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,6 +15,12 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
+
+	MyInt = 9;
+
+	APlusB = MyIntA + MyIntB;
+
+	APlusB = MyInt + MyIntB;
 	
 }
 
--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -27,6 +27,15 @@ public:
 	int32 MyInt = 99;
 
 	UPROPERTY(EditAnywhere)
+	int32 MyIntA = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 MyIntB = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 APlusB = 0;
+
+	UPROPERTY(EditAnywhere)
 	bool MyBool = false;
 
 };
